b016c0b9093d1e8666892dfd637b1feacc5da442
Connor <crnbarr@gmail.com>
2018-09-27 18:09:08 +0100

    Updated transaction js to exit on transaction sent

--- transaction.js
+++ transaction.js
@@ -2,9 +2,6 @@ var Web3 = require('web3');
 var web3 = new Web3('ws://localhost:8545');
 
 var ContractAddress = process.argv[2];
-
-console.log('CONTRACT ADDRESS:', ContractAddress);
-
 var ABIString = '[{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"data","type":"uint256"}],"name":"ItBlinks","type":"event"}]';
 	
 //  Use the string and convert to a JSON object - ABI
@@ -12,9 +9,6 @@ var ABI = JSON.parse(ABIString);
 
 // now retrieve your contract object with the ABI and contract address values
 var blinker = new web3.eth.Contract(ABI, ContractAddress);
-
-if (blinker) console.log('CONTRACT INSTANTIATED SUCCESSFULLY!');
-
 var coinbase;
 //Async call to retrieve coinbase account
 var getCoinbase = async () => {
@@ -24,10 +18,13 @@ var getCoinbase = async () => {
 }
 
 getCoinbase().then(() => {
+  console.log('SENDING TRANSACTION...');
 	blinker.methods.set(1).send({
 		from: coinbase,
-	});
-	
-	console.log('SENDING TRANSACTION...');
+	}).then((receipt) => {
+    if (!receipt) console.error('Somethign went wrong, no receipt received')
+    if (receipt) console.log('TRANSACTION SENT');
+    process.exit();
+  });
 })
 
